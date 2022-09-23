f395e84086006d00a43c8248bd271376078b529a
grxuau <henryycampbell@gmail.com>
2022-09-23 10:18:08 +0300

    some fixes

--- src/server/Server.java
+++ src/server/Server.java
@@ -27,12 +27,12 @@ public class Server {
     private static ConsoleThread consoleThread;
 
     public static void main(String[] args) {
-//        if (args.length == 0) {
-//            System.out.println("Имя файла не поступило, остановка...");
-//            System.exit(0);
-//        }
+        if (args.length == 0) {
+            System.out.println("Имя файла не поступило, остановка...");
+            System.exit(0);
+        }
 
-        fileManager = new FileManager("src/collection.csv");
+        fileManager = new FileManager(args[0]);
         collectionManager = new CollectionManager(fileManager);
         responseSender = new ResponseSender(collectionManager);
         consoleThread = new ConsoleThread(collectionManager);
